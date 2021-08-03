35f2f2ea232e17a72b9513a7ab5a22225f1beaa5
Hayden <haydenwarren2003@gmail.com>
2021-08-03 19:40:28 +0000

    sign in updated

---
+++ public/js/signOut.js
@@ -0,0 +1,7 @@
+const signOut = () => {
+    firebase.auth().signOut().then(function() {
+    console.log('Signed Out');
+    }, function(error) {
+    console.error('Sign Out Error', error);
+    });
+}
