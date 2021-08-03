800d81e72aeda80d23361839dcd8ac4202477e4f
Hayden <haydenwarren2003@gmail.com>
2021-08-03 20:22:44 +0000

    sign in updated/ firebase.auth not a function back

--- public/js/signOut.js
+++ public/js/signOut.js
@@ -1,5 +1,6 @@
 const signOut = () => {
     firebase.auth().signOut().then(function() {
+    window.location = 'signin.html';
     console.log('Signed Out');
     }, function(error) {
     console.error('Sign Out Error', error);
